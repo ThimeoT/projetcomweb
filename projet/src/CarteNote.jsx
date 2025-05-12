13e049bf8caf0cd59f2a119d89c6058781a2cb5d
Thiméo Tonon <ttonon@ensc.fr>
2025-05-12 16:42:29 +0200

    changements sur l'affichage

--- projet/src/CarteNote.jsx
+++ projet/src/CarteNote.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 function CarteNote({ date, matiere, note_obtenue, bareme }) {
     return (
-        <div style={{ border: "1px solid #ccc", margin: "10px", padding: "10px", borderRadius: "8px" }}>
+        <div style={{ border: "3px solid #ccc", margin: "10px", padding: "10px", borderRadius: "8px",borderColor: "#dddddd" }}>
             <div><strong>Date :</strong> {date}</div>
             <div><strong>Matière :</strong> {matiere}</div>
             <div><strong>Note :</strong> {note_obtenue} / {bareme}</div>
